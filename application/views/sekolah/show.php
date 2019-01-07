e09a6ea36da01b0b503e60476d1bc272057eb64c
Yudhapradana <yudhapradana102@gmail.com>
2019-01-07 10:24:24 +0700

    sekolah

--- application/views/sekolah/show.php
+++ application/views/sekolah/show.php
@@ -1,12 +1,12 @@
 <div class="modal-body">
-  <h5>NPSP</h5>
-  <p><?php echo $sekolah->npsp; ?></p>
+  <h5>NPSN</h5>
+  <p><?php echo $sekolah->npsn; ?></p>
   <hr>
   <h5>Nama Sekolah</h5>
   <p><?php echo $sekolah->nama_sekolah; ?></p>
   <hr>
   <h5>Jenis & Status</h5>
-  <p><?php echo $sekolah->jenis_sekolah; ?> <?php echo $sekolah->status_sekolah; ?></p>
+  <p><?php echo $sekolah->nama_jenis_sekolah; ?> <?php echo $sekolah->nama_status_sekolah; ?></p>
   <hr>
   <h5>Alamat</h5>
   <p><?php echo $sekolah->alamat ?></p>
